bf469daa4631ba94abeaaf5f17ffd52493ad760a
Kr1kArtyom <ignatev.artemon@yandex.ru>
2024-04-21 21:52:29 +0300

    Игнатьев Артем ПИпр

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -17,7 +17,7 @@ void Generator::createStatetab(std::istream &source, int prefixSize) {
 std::string Generator::generateText(int length) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    
+
     std::uniform_int_distribution<> dis(0, statetab.size() - 1);
     auto statetab_it = statetab.begin();
     std::advance(statetab_it, dis(gen));
@@ -31,10 +31,11 @@ std::string Generator::generateText(int length) {
     for (int i = 0; i < length - prefix.size(); i++) {
         if (statetab[prefix].empty()) break;
 
-        std::uniform_int_distribution<> dis_words(0, statetab[prefix].size() - 1);
+        std::uniform_int_distribution<> dis_words(0,
+            statetab[prefix].size() - 1);
         int random_index = dis_words(gen);
         std::string word = statetab[prefix][random_index];
-        
+
         res += word + " ";
         prefix.pop_front();
         prefix.push_back(word);
